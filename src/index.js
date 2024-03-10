70493a2a047b158db86cef1213383bbbcbe36717
Romario Melo <romario.melos@gmail.com>
2024-03-10 01:18:49 -0300

    refact function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ function hello(name) {
   return `Hello ${name}!`;
 }
 
-function sum(a, b) {
-  return a + b;
-}
-
 function sub(a, b) {
   return a - b;
+}
+
+function sum(a, b) {
+  return a + b;
 }
 
 module.exports = hello;
